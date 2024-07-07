70f8abe1327d3c923fad142c67055a35243060fa
Andrés Ortega <andres03ortega@gmail.com>
2024-07-07 19:42:19 +0200

    formulario en marcha

---
+++ src/app/api/sendEmail/route.js
@@ -0,0 +1,34 @@
+// route.js
+import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+
+export async function POST(request) {
+    try {
+        const { subject, message } = await request.json();
+
+        const transporter = nodemailer.createTransport({
+            service: 'hostinger',
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: process.env.SMTP_SECURE === 'true', // Asegúrate de convertir a booleano
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.SMTP_USER,
+            to: 'andres03ortega@gmail.com',
+            subject: subject || 'Envio de prueba',
+            html: message,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return NextResponse.json({ message: "Failed to Send Email" }, { status: 500 });
+    }
+}
--- src/app/components/nosotros-index/nosotros-form.jsx
+++ src/app/components/nosotros-index/nosotros-form.jsx
@@ -1,33 +1,45 @@
-// components/ContactForm.js
-
 import React, { useState } from 'react';
 
-const ContactForm = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        tel: '',
-    });
+const NosotrosForm = () => {
+    const [formData, setFormData] = useState({ name: '', email: '', tel: '', message: '' });
+    const [status, setStatus] = useState('');
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await fetch('/api/sendEmail', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
-        if (res.ok) {
-            alert('Mensaje enviado correctamente!');
-            setFormData({ name: '', email: '', tel: '' });
-        } else {
-            alert('Error al enviar el mensaje.');
+        setStatus(''); // Reset status
+
+        try {
+            const response = await fetch('/api/sendEmail', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    subject: `Mensaje de ${formData.name}`,
+                    message: `
+                        <p>Nombre: ${formData.name}</p>
+                        <p>Email: ${formData.email}</p>
+                        <p>Teléfono: ${formData.tel}</p>
+                    `,
+                }),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                setStatus('success');
+                setFormData({ name: '', email: '', tel: '', message: '' }); // Clear form
+            } else {
+                setStatus('error');
+            }
+            console.log(data);
+        } catch (error) {
+            console.error('Error sending email:', error);
+            setStatus('error');
         }
     };
 
@@ -36,21 +48,54 @@ const ContactForm = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Nombre completo</label>
-                    <input type="text" id="name" name="name" placeholder="Pon tu nombre y apellidos" value={formData.name} onChange={handleChange} required />
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Pon tu nombre y apellidos"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
                 </div>
                 <div>
                     <label htmlFor="email">Correo electrónico</label>
-                    <input type="email" id="email" name="email" placeholder="¿Cuál es tu mail?" value={formData.email} onChange={handleChange} required />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="¿Cuál es tu mail?"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
                 </div>
                 <div>
-                    <label htmlFor="message">Teléfono</label>
-                    <input type="tel" id="phone" name="tel" maxLength="9" placeholder="¿Te podemos llamar?" inputMode="numeric" value={formData.tel} onChange={handleChange} required />
+                    <label htmlFor="tel">Teléfono</label>
+                    <input
+                        type="tel"
+                        id="tel"
+                        name="tel"
+                        maxLength="9"
+                        placeholder="¿Te podemos llamar?"
+                        inputMode="numeric"
+                        value={formData.tel}
+                        onChange={handleChange}
+                        required
+                    />
                 </div>
-                <p className="nosotros__form-aviso">Al hacer clic en ENVIAR estás aceptando la política de privacidad.</p>
-                <button className="nosotros__form-button" type="submit">Enviar</button>
+               
+                <p className="nosotros__form-aviso">
+                    Al hacer clic en ENVIAR estás aceptando la política de privacidad.
+                </p>
+                <button className="nosotros__form-button" type="submit">
+                    Enviar
+                </button>
             </form>
+            {status === 'success' && <p className="success-message">¡Formulario enviado con éxito!</p>}
+            {status === 'error' && <p className="error-message">Hubo un error al enviar el formulario. Inténtalo de nuevo.</p>}
         </div>
     );
 };
 
-export default ContactForm;
+export default NosotrosForm;
