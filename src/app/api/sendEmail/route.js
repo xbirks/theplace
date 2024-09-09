f9d0b937d2536d2f2c9f45b855c0a2e3ad932994
Andrés Ortega <163862369+xbirks@users.noreply.github.com>
2024-09-09 09:17:06 +0200

    cambios form

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -26,16 +26,16 @@ export async function POST(request) {
 
         const mailOptions = {
             from: process.env.SMTP_USER,
-            to: 'andres03ortega@gmail.com, aom.contacto@gmail.com',
-            subject: subject || 'Envio de prueba',
+            to: 'neus@placevlc.com, hugo@placevlc.com',
+            subject: subject || 'Solicitud de contacto',
             html: message,
         };
 
         await transporter.sendMail(mailOptions);
 
-        return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
+        return NextResponse.json({ message: "Email enviado correctamente" }, { status: 200 });
     } catch (error) {
         console.error('Error sending email:', error);
-        return NextResponse.json({ message: "Failed to Send Email" }, { status: 500 });
+        return NextResponse.json({ message: "Error al enviar el email" }, { status: 500 });
     }
 }
